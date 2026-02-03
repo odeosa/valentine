3f9dc93fec0d33e61160b28d137c0ab9abe1534e
Brian Omoruyi <62952986+odeosa@users.noreply.github.com>
2026-02-04 00:51:23 +0100

    Update page.tsx styling for success

--- app/page.tsx
+++ app/page.tsx
@@ -192,7 +192,13 @@ export default function ValentinePage() {
             </div>
           </>
         ) : (
-          <h2 style={styles.success, styles.successOverlay}>Yayyy 💖 Thank you for accepting! You have made me the happiest person in the world! 💕🕺🏽</h2>
+          <h2 style={{...styles.success,  position: "absolute",
+  inset: "0",
+  background: "white",
+  zIndex: "50",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",}}>Yayyy 💖 Thank you for accepting! You have made me the happiest person in the world! 💕🕺🏽</h2>
         )}
       </div>
     </main>
@@ -273,15 +279,6 @@ const styles: Record<string, React.CSSProperties> = {
     padding: "0.8rem 2rem",
     cursor: "pointer",
     transition: "all 0.25s ease",
-  },
-  successOverlay: {
-  position: "absolute",
-  inset: "0",
-  background: "white",
-  zIndex: "50",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
   },
   success: {
     fontSize: "1.8rem",
